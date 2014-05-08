fa2c466905b6504ff22b9012d5d3ffaf6709dcc3
Nirman Dave <nirman.dave@hotmail.com>
2014-05-08 18:30:29 +0530

    Create Form6-About.vb

---
+++ Form6-About.vb
@@ -0,0 +1,13 @@
+Public Class Form6
+    Private Sub LinkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        System.Diagnostics.Process.Start("http://www.nirman.uni.me")
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        System.Diagnostics.Process.Start("http://www.sourcenet.blogspot.com")
+    End Sub
+
+    Private Sub LinkLabel3_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel3.LinkClicked
+        System.Diagnostics.Process.Start("http://www.softpedia.com/progMoreBy/Publisher-Nirman-Dave-75689.html")
+    End Sub
+End Class
