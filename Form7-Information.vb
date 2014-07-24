a9c3ee683b5ec9f3e904c58e4c6720a5a277c13d
Nirman Dave <nirman.dave@hotmail.com>
2014-07-24 17:32:38 +0530

    Update and rename Form7-Information to Form7-Information.vb

--- Form7-Information.vb
+++ Form7-Information.vb
@@ -4,6 +4,6 @@ Public Class Form7
     End Sub
 
     Private Sub LinkLabel2_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
-        System.Diagnostics.Process.Start("http://www.sourcenet.blogspot.com")
+        System.Diagnostics.Process.Start("http://www.sourcenet.in")
     End Sub
 End Class
